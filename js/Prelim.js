d52f0fcd22c0e07a06a950462d64c2a80761ef21
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2023-01-21 22:03:22 +0800

    update step 2

--- js/Prelim.js
+++ js/Prelim.js
@@ -1,7 +1,11 @@
 const addQuizzes = document.getElementById('add-quizzes')
 let currentStep = 1
-let count = 0 
+let Q_count = 0 
 let quizArray = []
+const prelim_form = {'Pquiz':0,'Pcs':0,'Pil':0,'Plecme':0,'ES':0}
+const result=()=>{
+
+}
 
 const notify = (msg)=>{
     notification.style.display = 'flex'
@@ -13,11 +17,46 @@ const notify = (msg)=>{
 
 
 const nextStep=(step)=> {
-	for(i=0;i<=count;i++){
-		let quiz = document.getElementById(`quiz${i}`)
-		console.log(Number(quiz.value))
+	if(step == 2){
+		let Pre_QG = 0
+		for(i=0;i<=Q_count;i++){
+			let quiz = document.getElementById(`quiz${i}`)
+			Pre_QG+=Number(quiz.value)
+		}
+		prelim_form['Pquiz'] = Q_count+1 > 1 ? Number((Pre_QG / (Q_count+1)).toFixed(2)) : Number(Pre_QG.toFixed(2))
+		console.log(prelim_form,Q_count)
+		Validate(AveQuiz(),step)
 	}
-	if(validateNumber()){
+	if(step == 3){
+		
+		let PLec_CS = document.getElementById('PLec_CS').value
+		let PLec_IL = document.getElementById('PLec_IL').value
+		let Pre_WritExamItems = document.getElementById('Pre_WritExamItems').value
+		let Pre_WritExamScore = document.getElementById('Pre_WritExamScore').value
+
+
+		prelim_form['Pcs'] = ((isNaN(PLec_CS)) || Number(PLec_CS) < 0 || Number(PLec_CS) > 100 ) ? -1 : Number(PLec_CS)
+
+		prelim_form['Pil'] = ((isNaN(PLec_IL)) || Number(PLec_IL) < 0 || Number(PLec_IL) > 100 ) ? -1 : Number(PLec_IL)
+
+		prelim_form['Plecme'] = ((isNaN(Pre_WritExamItems)) || Number(Pre_WritExamItems) < 0 || Number(Pre_WritExamItems) > 100 ) ? -1 : Number(Pre_WritExamItems)
+
+		prelim_form['ES'] =  ((isNaN(Pre_WritExamScore)) || prelim_form['Plecme']<Number(Pre_WritExamScore) || Number(Pre_WritExamScore) < 0 || Number(Pre_WritExamScore) > 100 ) ? -1 : Number(Pre_WritExamScore)
+		console.log(prelim_form)
+		if(prelim_form['Pcs'] > 0 && prelim_form['Pil'] > 0 && prelim_form['Plecme'] && prelim_form['ES']){
+			Validate(true,step)
+		}else{
+			notify('Invalid Score')
+		}
+	}
+	if(step == 4){
+		Validate(true,step)
+	}
+}
+
+
+const Validate=(isTrue,step)=>{
+	if(isTrue){
 		document.getElementById(`step${currentStep}`).classList.remove("active")
 		currentStep = step;
 		document.getElementById(`step${currentStep}`).classList.add("active")
@@ -35,115 +74,26 @@ const prevStep=(step)=>{
 document.getElementById("step1").classList.add("active")
 
 const AddQuizBtn= ()=>{
-	count++
+	Q_count++
 	addQuizzes.innerHTML+=`
 	<div class="input-container" id="add-quizzes">
-	<label for="quiz${count}">Quiz ${count+1}:</label>
-	<input type="text" type='number' placeholder='Enter score for quiz number ${count+1}' id="quiz${count}" name="quiz${count}">
+	<label for="quiz${Q_count}">Quiz ${Q_count+1}:</label>
+	<input type="text" type='number' placeholder='Enter score for quiz number ${Q_count+1}' id="quiz${Q_count}" name="quiz${Q_count}">
 	</div>`
 }
 
-const validateNumber=()=>{
+const AveQuiz=()=>{
 	let counter = 0
-	for(i=0;i<=count;i++){
+	for(i=0;i<=Q_count;i++){
 		let quiz = document.getElementById(`quiz${i}`).value
 		if(Number(quiz)>=0 && Number(quiz)<=100){
 			counter++
 		}
 	}
-	return count+1 == counter
+	return Q_count+1 == counter
 }
 
-// //-------------------------------------------- START OF LECTURE GRADE
-// 		//START OF QUIZ
-// 		var Pre_QG = 0;
-//         // check if the user input is number
-//         while(true){ 
-//             Q_count = Number(prompt("LECTURE: How many Quiz did you conduct?"));
-//             if(isNaN(Q_count)) alert("Letter or special character are not accepted") 
-//             else break
-//         }
 
-// 		x = Q_count + 1;
-
-// 		i = 1; //initialize variable
-// 		while(i != x){ //condition
-// 			a = parseFloat(prompt("Enter grade for Q" + i));
-// 			if (isNaN(a)){
-// 				alert("Letters and Special Characters are not accepted.");}
-// 			else if(a < 0 || a > 100){
-// 				alert("Negative numbers and numbers higher than 100 and invalid.");}
-// 			else{
-// 				i++; //iteration
-// 				Pre_QG += a;}
-// 		}
-
-// 		if (Q_count > 1){
-// 			Pre_QG = (Pre_QG / Q_count).toFixed(2);
-// 			prelim_form.Pquiz.value = Pre_QG;} //get the average
-// 		else{
-// 			prelim_form.Pquiz.value = Pre_QG.toFixed(2);
-// 		}
-// 		//END OF QUIZ
-
-// 		//START OF CLASS STANDING
-// 		while(true){
-// 			PLec_CS = Number(prompt("LECTURE: Enter grade for Class Standing"));
-// 			if (isNaN(PLec_CS)){
-// 				alert("Invalid score!");
-// 			}
-// 			else if(PLec_CS < 0 || PLec_CS > 100){
-// 				alert("Invalid score!");
-// 			}
-// 			else{
-// 				prelim_form.Pcs.value = PLec_CS.toFixed(2);
-// 				break;}
-// 		}//END OF CLASS STANDING
-
-// 		//START OF INDEPENDENT LEARNING
-// 		while(true){
-// 			PLec_IL = Number(prompt("LECTURE: Enter grade for Indepenent Learning"));
-// 			if (isNaN(PLec_IL)){
-// 				alert("invalid score");
-// 			}
-// 			else if(PLec_IL < 0 || PLec_CS > 100){
-// 				alert("invalid score");
-// 			}
-// 			else{
-// 				prelim_form.Pil.value = PLec_IL.toFixed(2);
-// 				break;
-// 			}
-// 		}//END OF INDEPENDENT LEARNING
-
-// 		//START OF MAJOR EXAM
-// 		Pre_WritExamScore = 0;
-// 		Pre_WritExamItems = Number(prompt("LECTURE: Enter number of items for written exam."));
-// 		while(true){
-// 			Pre_WritExamScore = Number(prompt("LECTURE: Enter score"));
-// 			if (isNaN(Pre_WritExamScore)){
-// 				alert("Invalid score!");
-// 			}
-// 			else if(Pre_WritExamScore < 0){
-// 				alert("Invalid score!");
-// 			}
-// 			else if (Pre_WritExamScore > Pre_WritExamItems){
-// 				alert("Invalid score!");
-// 			}
-// 			else
-// 				break;
-// 		}
-// 		Pre_Writ_ME = (Pre_WritExamScore / Pre_WritExamItems) * 100;
-// 		prelim_form.Plecme.value = Pre_Writ_ME.toFixed(2);
-// 		//END OF MAJOR EXAM
-
-// 		//START OF LECTURE GRADE
-// 		Pre_QG = Pre_QG * .3;
-// 		PLec_CS = PLec_CS * .1;
-// 		PLec_IL = PLec_IL * .1;
-// 		Pre_Writ_ME = Pre_Writ_ME *.5;
-// 		PLECGRADE = Pre_QG + PLec_IL + PLec_CS + Pre_Writ_ME;
-// 		prelim_form.PLEC_G.value = PLECGRADE.toFixed(2);
-// 		//END OF LECTURE GRADE
 
 // 		//-------------------------------------------- START OF LABORATORY GRADE
 // 		//START OF QUIZ
