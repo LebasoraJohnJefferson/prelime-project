b5f4d41e48d707d60939ddbd7f88f56c1a0f2602
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2023-01-21 11:51:26 +0800

    calculation status

--- js/grade.js
+++ js/grade.js
@@ -6,6 +6,8 @@ const quizzesInput = document.getElementById('form')
 const otherInput = document.getElementById('form2')
 const notification = document.getElementById('notification')
 const animation = document.getElementById('animation')
+const congratsAnimation = document.getElementById('congrats')
+const gradeStatus = document.getElementById('grade-status')
 
 let LEC_Q = 0
 
@@ -18,9 +20,26 @@ const notify = (msg)=>{
     },3000)
 }
 
+const back=()=>{
+    animation.style.display = 'flex'
+    if(form2[0]) form2[0].style.display = "none"
+    setTimeout(()=>{
+        animation.style.display = 'none'    
+        if(form[0]) form[0].style.display = "flex"
+    },2000)
+}
+
 const createHTMLForm =()=>{
     if(start[0]) start[0].style.display="none"
     if(form[0]) form[0].style.display = "flex"
+}
+
+const congrats=()=>{
+    congratsAnimation.style.display = 'flex'
+}
+
+const confirmation=()=>{
+    congratsAnimation.style.display = 'none'
 }
 
 const SumOfQuizzes=(e)=>{
@@ -56,15 +75,29 @@ const Calculate=()=>{
     if( Number(data2['LEC_CS']<0) | Number(data2['LEC_IL'] < 0 ) | Number(data2['WE_items'])<0 | Number(data2['WE_score'])<0 | Number(data2['LEC_CS']>100) | Number(data2['LEC_IL'] > 100 )){
         notify('Input must be greater than 1 and less than 100')
     }else{
+        let status=''
         if(Number(data2['WE_items']) >= Number(data2['WE_score'])){
             LEC_CS = Number(data2['LEC_CS']) * .1
             LEC_IL = Number(data2['LEC_IL']) * .1
             WE_items =  Number(data2['WE_items']) == 0 ? 1 : Number(data2['WE_items'])
             LEC_ME = (parseFloat((Number(data2['WE_score']) / WE_items)) * 100) * .5
             LEC_G=parseFloat(LEC_Q + LEC_CS + LEC_IL + LEC_ME).toFixed(2)
-            
-            if (LEC_G >= 50 && LEC_G <= 100) alert("your grade is " + LEC_G + "\nyou have a passing grade");
-            else alert("your grade is " + LEC_G + "\nyou have a failing grade");
+            gradeStatus.classList.remove('pass')
+            gradeStatus.classList.remove('fail')
+            if(LEC_G >= 50 && LEC_G <= 100){
+                status = "Your grade is " + LEC_G + "\nyou have a passing grade"
+                gradeStatus.classList.add('pass')
+                congratsAnimation.style.backgroundImage='url(../assets/congrats.gif)'
+                gradeStatus.style.background = 'green'
+            }
+            else { 
+                status = "Your grade is " + LEC_G + "\nyou have a failing grade" 
+                congratsAnimation.style.backgroundImage='none'
+                gradeStatus.classList.add('fail')
+                gradeStatus.style.background = 'red'
+            }
+            gradeStatus.innerHTML = `<h3>${status}</h3>`
+            congrats()
         }else{
             notify('Item must be greater then score!')
         }
