7a45c4750b746dc000bd209fdcad54860d1b2d07
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2023-01-23 21:57:31 +0800

    finish

--- js/Prelim_data_entry.js
+++ js/Prelim_data_entry.js
@@ -7,6 +7,10 @@ const studentUSN = document.getElementById('student-USN')
 const studentLname = document.getElementById('student-Lname')
 const studentFname = document.getElementById('student-Fname')
 const studentMname = document.getElementById('student-Mname')
+const field = document.getElementById('field')
+const we_items = document.getElementById('we_items')
+const we_score = document.getElementById('we_score')
+
 const totalQuizScore = document.getElementById('totalQuizScore')
 const LecCS = document.getElementById('LecCS')
 const LecPil = document.getElementById('LecPil')
@@ -18,11 +22,63 @@ const Plabquiz = document.getElementById('Plabquiz')
 const Plabcsz = document.getElementById('Plabcsz')
 const Plabme = document.getElementById('Plabme')
 
+let tempTotalQuizScore = {}
+let tempLecCs={}
+let tempLecPil={}
+let tempPlecme = {}
+let tempPLAB_G={}
+let tempPRELIM_G={}
+let tempPLEC_G={}
+let tempPlabquiz={}
+let tempPlabcsz={}
+let tempPlabme={}
+let q1={}
+let q2={}
+let q3={}
+let ps={}
+let m1={}
+let m2={}
+let m3={}
+let tScore={}
+let tItems={}
+let fScore={}
+let fItems={}
+let context = 'PRELIM'
+
+
 let totalQuiz=0
 
-const show=(context)=>{
-    let redirect = context=='prelim' ? 'flex' : 'none'
-    prelimPeriodContainer.style.display = redirect
+const show=(tempContext)=>{
+    context=tempContext
+    LecPil.innerHTML=isNaN(tempLecPil[context]) ? '' : tempLecPil[context]
+    Plecme.innerHTML=isNaN(tempPlecme[context]) ? '' : tempPlecme[context]
+    PLAB_G.innerHTML=isNaN(tempPLAB_G[context]) ? '' : tempPLAB_G[context]
+    PRELIM_G.innerHTML=isNaN(tempPRELIM_G[context]) ? '' : tempPRELIM_G[context]
+    PLEC_G.innerHTML=isNaN(tempPLEC_G[context]) ? '' : tempPLEC_G[context]
+    Plabquiz.innerHTML=isNaN(tempPlabquiz[context]) ? '' : tempPlabquiz[context]
+    Plabcsz.innerHTML=isNaN(tempPlabcsz[context]) ? '' : tempPlabcsz[context]
+    Plabme.innerHTML=isNaN(tempPlabme[context]) ? '' : tempPlabme[context]
+    LecCS.innerHTML=isNaN(tempLecCs[context]) ? '' : tempLecCs[context]
+    field.innerHTML=context
+    totalQuizScore.innerHTML= isNaN(q1[context]) | isNaN(q3[context]) | isNaN(q2[context]) ? '' : ((Number(q1[context]) + Number(q2[context]) + Number(q3[context])) / 3).toFixed(2)
+    prelim_entry.q1.value =isNaN(q1[context]) ? '' : q1[context]
+    prelim_entry.q2.value =isNaN(q2[context]) ? '' : q2[context]
+    prelim_entry.q3.value =isNaN(q3[context]) ? '' : q3[context]
+    prelim_entry.Ps.value =isNaN(tempLecCs[context]) ? '' : tempLecCs[context]
+    prelim_entry.Pil.value=isNaN(tempLecPil[context]) ? '' : tempLecPil[context]
+    Plecme.innerHTML = isNaN(tempPlecme[context]) ? '' : tempPlecme[context]
+    PRELIM_G.innerHTML = isNaN(tempPRELIM_G[context]) ? '' : tempPRELIM_G[context]
+    prelim_entry.we_score.value = isNaN(tScore[context]) ? '' : tScore[context]
+    prelim_entry.we_items.value = isNaN(tItems[context]) ? '' : tItems[context]
+    prelim_entry.m1.value = isNaN(m1[context]) ? '' : m1[context]
+    prelim_entry.m2.value = isNaN(m2[context]) ? '' : m2[context]
+    prelim_entry.m3.value = isNaN(m3[context]) ? '' : m3[context]
+    Plabquiz.innerHTML = isNaN(tempPlabquiz[context]) ? '' : tempPlabquiz[context]
+    prelim_entry.Plabcs.value = isNaN(tempPlabcsz[context]) ? '' : tempPlabcsz[context]
+    Plabcsz.innerHTML = isNaN(tempPlabcsz[context]) ? '' : tempPlabcsz[context]
+    prelim_entry.PE_score.value = isNaN(fScore[context] ) ? '' : fScore[context] 
+    prelim_entry.PE_items.value = isNaN(fItems[context]) ? '' : fItems[context]
+    Plabme.innerHTML = isNaN(tempPlabme[context]) ? '' : tempPlabme[context]
 }
 
 const Save=()=>{
@@ -63,21 +119,21 @@ const notify = (msg)=>{
     },3000)
 }
 
-
 // calculation 
-
 const prelim_quiz=()=>{
     var Prelim_Q = 0;
-    q1 = prelim_entry.q1.value;
-    q2 = prelim_entry.q2.value;
-    q3 = prelim_entry.q3.value;
-    Prelim_Q = (parseFloat(q1) + parseFloat(q2) + parseFloat(q3)) / 3;
-    if((q1!='') && q2!='' && q3!=''){
-        if ((isNaN(Prelim_Q)) || (q1 < 0 || q1 > 100) || (q2 < 0 || q2 > 100) || (q3 < 0 || q3 > 100)){
+    q1[context] = prelim_entry.q1.value;
+    q2[context] = prelim_entry.q2.value;
+    q3[context] = prelim_entry.q3.value;
+    Prelim_Q = (parseFloat(q1[context]) + parseFloat(q2[context]) + parseFloat(q3[context])) / 3;
+    if((q1[context]!='') && q2[context] !='' && q3[context] !=''){
+        if ((isNaN(Prelim_Q)) || (q1[context] < 0 || q1[context] > 100) || (q2[context] < 0 || q2[context] > 100) || (q3[context] < 0 || q3[context] > 100)){
             notify("Invalid Entries")
-            totalQuizScore.innerHTML = 'Invalid Entries'
+            tempTotalQuizScore[context] = 'Invalid Entries'
+            return
         } 
-        else totalQuizScore.innerHTML = Prelim_Q.toFixed(2)
+        tempTotalQuizScore[context] = Prelim_Q.toFixed(2)
+        totalQuizScore.innerHTML = Prelim_Q.toFixed(2)
     }
 }
 
@@ -87,9 +143,12 @@ const prelim_cs=()=>{
     z = parseFloat(prelim_entry.Ps.value);
     if (isNaN(z) || (z < 0 || z > 100)){
         notify('Invalid Entries')
+        tempLecCs[context]='Invalid Entry'
         LecCS.innerHTML = "Invalid Entry";}
     else{
-        LecCS.innerHTML = z.toFixed(2);}
+        tempLecCs[context]=z.toFixed(2)
+        LecCS.innerHTML = z.toFixed(2)
+        ;}
 }
 
 const prelim_il=()=>{
@@ -97,17 +156,22 @@ const prelim_il=()=>{
     var z = 0;
     z = parseFloat(prelim_entry.Pil.value);
     if (isNaN(z) || (z < 0 || z > 100)){
+        tempLecPil[context] = "Invalid Entry"
         LecPil.innerHTML = "Invalid Entry"
         notify('Invalid Entry')
     }else{
+        tempLecPil[context]=z.toFixed(2)
         LecPil.innerHTML = z.toFixed(2)
     }	
 }
 
 const prelim_we=(score,items)=>{
     var res = 0;
+    tScore[context]=score
+    tItems[context]=items
     if (parseFloat(score) > parseFloat(items)){
         Plecme.innerHTML = "Invalid Entry";
+        tempPlecme[context]='Invalid Entry'
     }else{
         a = totalQuizScore.innerHTML;
         b = LecCS.innerHTML;
@@ -116,26 +180,32 @@ const prelim_we=(score,items)=>{
         f = PLAB_G.innerHTML;
         res = parseFloat(score/items) * 100;
         Plecme.innerHTML = res.toFixed(2);
+        tempPlecme[context]=res.toFixed(2)
     }
         d = (a * .3) + (b * .1) + (c * .1) + (res * .5);
         PLEC_G.innerHTML = d.toFixed(2);
+        tempPLEC_G[context] = d.toFixed(2)
         PRELIM_G.innerHTML = ((d * .4) + (f * .6)).toFixed(2);
+        tempPRELIM_G[context] =  ((d * .4) + (f * .6)).toFixed(2);
 }
 
 const prelim_LAB_quiz=()=>{
     var Prelim_mQ = 0;
-    m1 = prelim_entry.m1.value;
-    m2 = prelim_entry.m2.value;
-    m3 = prelim_entry.m3.value;
+    m1[context] = prelim_entry.m1.value;
+    m2[context] = prelim_entry.m2.value;
+    m3[context] = prelim_entry.m3.value;
 
-    Prelim_mQ = (parseFloat(m1) + parseFloat(m2) + parseFloat(m3)) / 3;
-    if(m1 == '' || m2=='' || m3 == '') return
-    if (isNaN(Prelim_mQ) || (m1 < 0 || m1 > 100) || (m2 < 0 || m2 > 100) || (m3 < 0 || m3 > 100)){
+    Prelim_mQ = (parseFloat(m1[context]) + parseFloat(m2[context]) + parseFloat(m3[context])) / 3;
+    if(m1[context] == '' || m2[context]=='' || m3[context] == '') return
+    if (isNaN(Prelim_mQ) || (m1[context] < 0 || m1[context] > 100) || (m2[context] < 0 || m2[context] > 100) || (m3[context] < 0 || m3[context] > 100)){
         Plabquiz.innerHTML = "Invalid Entries"
+        tempPlabquiz[context]='Invalid Entries'
         notify('invalid Entry')
-    ;}
+        ;}
     else{
-        Plabquiz.innerHTML = Prelim_mQ.toFixed(2);}
+        Plabquiz.innerHTML = Prelim_mQ.toFixed(2)
+        tempPlabquiz[context]=Prelim_mQ.toFixed(2)
+    }
 }
 
 const  prelim_lab_cs=()=>{
@@ -144,27 +214,30 @@ const  prelim_lab_cs=()=>{
     z = parseFloat(prelim_entry.Plabcs.value);
     if (isNaN(z) || (z < 0 || z > 100)){
         Plabcsz.innerHTML = "Invalid Entry"
+        tempPlabcsz[context] = "Invalid Entry"
     }
     else{
+        tempPlabcsz[context] = z.toFixed()
         Plabcsz.innerHTML = z.toFixed(2);}
 }
 
 const pre_PE = ()=>{
-    score = prelim_entry.PE_score.value
-    items = prelim_entry.PE_items.value
-    if(score == '' || items=='') return
-    score = parseFloat(score);
-    items = parseFloat(items);
-    if(score>items){
+    fScore[context] = prelim_entry.PE_score.value
+    fItems[context] = prelim_entry.PE_items.value
+    if(fItems[context] == '' || fScore[context]=='') return
+    fScore[context] = parseFloat(fScore[context]);
+    fItems[context] = parseFloat(fItems[context]);
+    if(fItems[context]<fScore[context]){
         notify('items must greater than score')
         return
     }
-    if(isNaN(score) || (score < 0 || score > 100) || isNaN(items) || (items < 0 || items > 100)){
+    if(isNaN(fScore[context]) || (fScore[context] < 0 || fScore[context] > 100) || isNaN(fItems[context]) || (fItems[context] < 0 || fItems[context] > 100)){
         Plabme.innerHTML = "Invalid Entry"
         notify('invalid Entry')
         return
     }
-    items = items == 0 ? 1 : items
-    Plabme.innerHTML = (score / items ) * 100
+    fItems[context] = fItems[context] == 0 ? 1 : fItems[context]
+    tempPlabme[context]=(fScore[context] / fItems[context] ) * 100
+    Plabme.innerHTML = (fScore[context] / fItems[context] ) * 100
 }
     
